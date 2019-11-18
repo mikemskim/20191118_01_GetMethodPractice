e3a7aa37b57e4bfc6cca20d641d13ee2895ac212
mikemskim <mskim@steconsulting.co.kr>
2019-11-18 21:07:50 +0900

    아이디 저장여부 체크 박스 ContextUtil 활용 연습

--- app/src/main/java/com/tj/a20191118_01_getmethodpractice/LoginActivity.kt
+++ app/src/main/java/com/tj/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.tj.a20191118_01_getmethodpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.tj.a20191118_01_getmethodpractice.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
@@ -17,10 +18,23 @@ class LoginActivity : BaseActivity() {
 
     override fun setupEvent() {
 
+        rememberIdCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+
+//            연습문제
+//            각각의 결과를 저장하고 상황에 맞는 토스트를 띄우자
+//            앱을 다시 킬때 체크했던 결과를 이어서 보여주자.
+            ContextUtil.setIsRemember(mContext, isChecked)
+            if (isChecked) {
+                Toast.makeText(mContext, "체크함", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(mContext, "체크안함", Toast.LENGTH_SHORT).show()
+            }
+        }
         loginBtn.setOnClickListener {
 
             if(rememberIdCheckBox.isChecked) {
-                Toast.makeText(mContext, "ID를 저장해야 합니다.", Toast.LENGTH_SHORT).show()
+                ContextUtil.setUserId(mContext, idEdt.text.toString())
+                Toast.makeText(mContext, "ID를 저장했습니다.", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(mContext, "ID를 저장 안함", Toast.LENGTH_SHORT).show()
             }
@@ -29,6 +43,10 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setValue() {
+//        저장되어 있는 아이디가 뭔지?
+        idEdt.setText(ContextUtil.getUserId(mContext))
+
+        rememberIdCheckBox.isChecked = ContextUtil.getIsRemember(mContext)
     }
 
 }
--- app/src/main/java/com/tj/a20191118_01_getmethodpractice/utils/ContextUtil.kt
+++ app/src/main/java/com/tj/a20191118_01_getmethodpractice/utils/ContextUtil.kt
@@ -11,6 +11,7 @@ class ContextUtil {
 
 //        사용자의 아이디를 저장하는 항목 이름
         val USER_ID = "USER_ID"
+        val ISREMEMBER = "ISREMEMBER"
 
         fun setUserId(context: Context, userId:String) {
 //            메모장 파일이름(PracticePrefference)을 실제로 여는 동작
@@ -24,6 +25,20 @@ class ContextUtil {
             var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
             return pref.getString(USER_ID, "")!!
+        }
+
+        fun setIsRemember(context: Context, isRemember:Boolean) {
+//            메모장 파일이름(PracticePrefference)을 실제로 여는 동작
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+//            메모장에 내용을 작성하고 저장하는 부분
+            pref.edit().putBoolean(ISREMEMBER, isRemember).apply()
+        }
+
+        fun getIsRemember(context: Context) : Boolean {
+            //            메모장 파일이름(PracticePrefference)을 실제로 여는 동작
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(ISREMEMBER, false)!!
         }
     }
 }
