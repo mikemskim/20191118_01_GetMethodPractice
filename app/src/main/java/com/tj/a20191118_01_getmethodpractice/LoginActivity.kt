fe7f76b8a033e06aefdc7feae2ca21be7340f695
mikemskim <mskim@steconsulting.co.kr>
2019-11-18 20:14:27 +0900

    로그인 버튼 누르면 아이디 저장여부 구분

--- app/src/main/java/com/tj/a20191118_01_getmethodpractice/LoginActivity.kt
+++ app/src/main/java/com/tj/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.tj.a20191118_01_getmethodpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
@@ -14,6 +16,16 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+
+        loginBtn.setOnClickListener {
+
+            if(rememberIdCheckBox.isChecked) {
+                Toast.makeText(mContext, "ID를 저장해야 합니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(mContext, "ID를 저장 안함", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     override fun setValue() {
